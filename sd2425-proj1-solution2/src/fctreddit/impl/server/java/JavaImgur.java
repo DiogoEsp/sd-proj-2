31c018f7e7ca963fa53db223bad75058677d0b2f
Diogo <d.esperanca@campus.fct.unl.pt>
2025-05-20 21:41:28 +0100

    Fiz uma nova classe que é a classe que vai usar imgur.

---
+++ sd2425-proj1-solution2/src/fctreddit/impl/server/java/JavaImgur.java
@@ -0,0 +1,130 @@
+package fctreddit.impl.server.java;
+
+import com.github.scribejava.apis.ImgurApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.oauth.OAuth20Service;
+import com.google.gson.Gson;
+import fctreddit.api.User;
+import fctreddit.api.imgur.data.BasicResponse;
+import fctreddit.api.imgur.data.CreateAlbumArguments;
+import fctreddit.api.imgur.data.ImageUploadArguments;
+import fctreddit.api.java.Image;
+import fctreddit.api.java.Result;
+import java.io.IOException;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import fctreddit.impl.client.UsersClient;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+public class JavaImgur implements Image {
+
+    private static final String apiKey = "7acbc7e0d5ce8fa";
+    private static final String apiSecret = "e6c579220e16ceee0ff776b336a3b63a4a0c4c96";
+    private static final String accessTokenStr = "97847fa95a42a56a0bd792fcb4273f16a27fc871";
+
+    private static final String CREATE_ALBUM_URL = "https://api.imgur.com/3/album";
+    private static final String UPLOAD_IMAGE_URL = "https://api.imgur.com/3/image";
+    private static final String ADD_IMAGE_TO_ALBUM_URL = "https://api.imgur.com/3/album/{{albumHash}}/add";
+    private static final String GET_ALBUM_URL = "https://api.imgur.com/3/album/{{albumHash}}";
+    private static final String GET_IMAGE_URL = "https://api.imgur.com/3/image/{{imageId}}";
+    private static final int HTTP_SUCCESS = 200;
+    private static final String CONTENT_TYPE_HDR = "Content-Type";
+    private static final String JSON_CONTENT_TYPE = "application/json; charset=utf-8";
+    private static final String ALBUM_NAME = "SD";
+
+    private final Gson json;
+    private final OAuth20Service service;
+    private final OAuth2AccessToken accessToken;
+
+    class AlbumListResponse {
+        private List<Album> data;
+        public List<Album> getData() { return data; }
+    }
+
+    // Album object
+    class Album {
+        private String id;
+        private String title;
+        public String getId() { return id; }
+        public String getTitle() { return title; }
+    }
+
+    public JavaImgur(){
+        json = new Gson();
+        accessToken = new OAuth2AccessToken(accessTokenStr);
+        service = new ServiceBuilder(apiKey).apiSecret(apiSecret).build(ImgurApi.instance());
+    }
+
+    @Override
+    public Result<String> createImage(String userId, byte[] imageContents, String password) throws IOException, ExecutionException, InterruptedException {
+       /** Result<User> owner = getUsersClient().getUser(userId, password);
+
+        if (!owner.isOK())
+            return Result.error(owner.error());*/
+
+        String id = null;
+
+        // 1. Verificar se o álbum existe
+        String albumId = null;
+        OAuthRequest listAlbumsRequest = new OAuthRequest(Verb.GET, "https://api.imgur.com/3/account/me/albums/");
+        service.signRequest(accessToken, listAlbumsRequest);
+        Response listAlbumsResponse = service.execute(listAlbumsRequest);
+
+        if (listAlbumsResponse.getCode() == HTTP_SUCCESS) {
+            AlbumListResponse albums = json.fromJson(listAlbumsResponse.getBody(), AlbumListResponse.class);
+            for (Album album : albums.getData()) {
+                if (ALBUM_NAME.equals(album.getTitle())) {
+                    albumId = album.getId();
+                    break;
+                }
+            }
+        }
+
+        // Se não existir, criar o álbum
+        if (albumId == null) {
+            OAuthRequest createAlbumReq = new OAuthRequest(Verb.POST, CREATE_ALBUM_URL);
+            createAlbumReq.addHeader(CONTENT_TYPE_HDR, JSON_CONTENT_TYPE);
+            createAlbumReq.setPayload(json.toJson(new CreateAlbumArguments(ALBUM_NAME, ALBUM_NAME)));
+            service.signRequest(accessToken, createAlbumReq);
+            Response createAlbumResp = service.execute(createAlbumReq);
+
+            if (createAlbumResp.getCode() != HTTP_SUCCESS) {
+                return Result.error(Result.ErrorCode.INTERNAL_ERROR);
+            }
+
+            BasicResponse albumResp = json.fromJson(createAlbumResp.getBody(), BasicResponse.class);
+            albumId = albumResp.getData().get("id").toString();
+        }
+
+        // 2. Fazer upload da imagem
+        ImageUploadArguments uploadArgs = new ImageUploadArguments(imageContents, "imageTitle" );
+        OAuthRequest uploadReq = new OAuthRequest(Verb.POST, UPLOAD_IMAGE_URL);
+        uploadReq.addHeader(CONTENT_TYPE_HDR, JSON_CONTENT_TYPE);
+        uploadReq.setPayload(json.toJson(uploadArgs));
+        service.signRequest(accessToken, uploadReq);
+        Response uploadResp = service.execute(uploadReq);
+
+        if (uploadResp.getCode() != HTTP_SUCCESS) {
+            return Result.error(Result.ErrorCode.INTERNAL_ERROR);
+        }
+
+        BasicResponse uploadResponse = json.fromJson(uploadResp.getBody(), BasicResponse.class);
+        String imageId = uploadResponse.getData().get("id").toString();
+
+        // 3. Retornar o ID da imagem
+        return Result.ok(imageId);
+    }
+
+    @Override
+    public Result<byte[]> getImage(String userId, String imageId) {
+        return null;
+    }
+
+    @Override
+    public Result<Void> deleteImage(String userId, String imageId, String password) {
+        return null;
+    }
+}
